5299e37359088de85ce4beea4ac25ad9bad9abd7
AbdulGafar Olamide Ajao <gafarolamide.ajao@gmail.com>
2020-10-08 23:18:35 +0100

    -removed useless code

--- plugins/apollo-config.js
+++ plugins/apollo-config.js
@@ -2,31 +2,8 @@ export default (context, inject) => {
   return {
     httpEndpoint:
       'https://realm.mongodb.com/api/client/v2.0/app/eventimeline-jlgbq/graphql',
-    // httpLinkOptions: {
-    //   headers: {
-    //     apiKey:
-    //       'jjESSEtXgN6LN62HJklczqLsnYSOwsrdArjte2fncINdriB4yyrNI6hwnB7FSVDh',
-    //   },
-    // },
-    // override HTTP endpoint in browser only
-    // browserHttpEndpoint: '/graphql',
-    // You can use `wss` for secure connection (recommended in production)
-    // Use `null` to disable subscriptions
-    // wsEndpoint: 'ws://localhost:4000',
-
-    // LocalStorage token
     tokenName: 'apollo-token',
-
-    // Enable Automatic Query persisting with Apollo Engine
     persisting: true,
-
-    // Use websockets for everything (no HTTP)
-    // You need to pass a `wsEndpoint` for this to work
     websocketsOnly: false,
-    /*
-     * For permanent authentication provide `getAuth` function.
-     * The string returned will be used in all requests as authorization header
-     */
-    // getAuth: () => `Bearer ${$realmAppUser.accessToken}`,
   }
 }
